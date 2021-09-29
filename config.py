21dd0199a77dceee054e79553d7dcaacd8f6bdc8
Herve le bonheur <hrvhounkanrin@gmail.com>
2021-09-29 10:44:23 +0100

    commutations factors added

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
 import json
-CIMA_H=[]
+CIMA_H = []
 
 def load_cima_h():
-    with open('cima_h.json') as data_file:
-        CIMA_H = json.load(data_file)
+  with open('cima_h.json','r') as data_file:
+    CIMA_H = json.load(data_file)
 
 
-def __main__():
-    print('start loading')
-    load_cima_h()
-    print(CIMA_H)
+
+
+if __name__ == '__main__':
+  print('Trigger main')
+  load_cima_h()
